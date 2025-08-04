b2eb280ca17b10265ae7a06c05bdec39c8033348
patpatella <patiencepatella@gmail.com>
2025-08-04 21:11:31 +0200

    Added my_oop_practice.py

--- my_oop_practice.py
+++ my_oop_practice.py
@@ -25,4 +25,27 @@ hero1 = Superhero("The Flash", "Electric Surge", "Neo-Tokyo")
 hero2 = FlyingHero("Superwoman", "Wind Cutter", "Cloud Realm", 750)
 
 print(hero1.introduce())      
-print(hero2.defend())         
+print(hero2.defend())      
+
+class Vehicle:
+    def move(self):
+        raise NotImplementedError("Subclasses must implement move()")
+
+class Car(Vehicle):
+    def move(self):
+        return "Driving on the highway 🚗"
+
+class Plane(Vehicle):
+    def move(self):
+        return "Flying in the clouds ✈️"
+
+class Boat(Vehicle):
+    def move(self):
+        return "Sailing the seas 🚤"
+
+# Example usage
+vehicles = [Car(), Plane(), Boat()]
+
+for v in vehicles:
+    print(v.move())
+
